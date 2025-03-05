d01a39d5503600962dab504ebd6f4f6ba50ab7fc
Melisa Şimşek <melisasimsek@Melisa-MacBook-Air.local>
2025-03-05 14:27:04 +0300

    first commit

---
+++ Magic-8-Ball/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,23 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var viewImage1: UIImageView!
+    @IBAction func pressButton(_ sender: UIButton) {
+        let magicBall = [
+            UIImage(named: "ball1"),
+             UIImage(named: "ball2"),
+             UIImage(named: "ball3"),
+             UIImage(named: "ball4"),
+             UIImage(named: "ball5")]
+        
+       viewImage1.image =
+            magicBall [ Int.random(in: 0...4)  ]
+    }
+
+
+
+}
+
